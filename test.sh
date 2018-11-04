db311971f76d78278093242194a854890295d542
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-11-04 18:06:45 -0500

    Better test script

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mkdir -p build/output
+mkdir -p build/output/diff
 
 for path in $(find scenes -name "*.scn")
 do
     file=$(basename "$path")
-    build/tp2 "$path" build/output/"${file%.*}".png
+    (build/tp2 "$path" build/output/"${file%.*}".png &&
+        compare -compose src ../imageReferences/"${file%.*}".png build/output/"${file%.*}".png build/output/diff/"${file%.*}".png) &
 done
